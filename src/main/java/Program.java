e81ad866633720c476769fc8f75b0b235dfbf448
Eduardo Queiroz <eduhmq@gmail.com>
2025-05-03 23:05:00 -0300

    first commit

---
+++ src/main/java/Program.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        File file = new File("C:/Users/Eduxt/workspace/study/JavaStudies/FilesWithJava/textfile.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+}
